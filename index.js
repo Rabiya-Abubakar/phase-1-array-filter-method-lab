7a6d8e207361178ec508f27e7703d6328c5c01a2
+ <+>
2024-07-26 13:15:36 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+// Function to return a driver if the beginning provided letters match
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+
+// Function to access the data structure to check if the name matches
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
